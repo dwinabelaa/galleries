f69d5ad4fbbce29811e3bd652c326a400a4eaa53
dwinabelaa <dwinabela0910@gmail.com>
2023-03-24 12:52:17 +0700

    merapikan source code

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,16 +9,29 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @foreach ($home as $item)
-                        <div class="d-flex">
-                            <div class="flex-fill">
-                                <td>{{ $item['name'] }}</td>
-                            </div>
-                            <div class="flex-fill col-md-3 p-2">
-                                <td>{{ $item->getFirstMedia('image') }}</td>
-                            </div>
-                        </div>
-                    @endforeach
+                    <div class="d-flex justify-content-center">
+                        <table>
+                            <thead>
+                                <tr class="text-center">
+                                    <th>Name</th>
+                                    <th>Image</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($home as $item)
+                                    <tr>
+                                        <div>
+                                            <td>{{ $item['name'] }}</td>
+                                        </div>
+                                        <div>
+                                            <td><img src="{{ $item->getFirstMediaUrl('image') }}"
+                                                    style="max-width: 400px; height: auto"></td>
+                                        </div>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
